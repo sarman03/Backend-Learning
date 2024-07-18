bb07efea1647a4375602b44784bfb931785b5d1f
Sarmanpreet singh <131851235+sarman03@users.noreply.github.com>
2024-07-18 17:28:55 +0000

    deploy kearning

---
+++ index.js
@@ -0,0 +1,16 @@
+require ('dotenv').config();
+const express= require('express');
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/twitter', (req, res) => {
+    res.send('Hello Twitter oie!');
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on port ${port}`);
+})
